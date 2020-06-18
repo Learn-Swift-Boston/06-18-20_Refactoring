e4c6991818a3ec2c13a063423869f68e9d8c3e33
Matt Dias <matt.dias6@gmail.com>
2020-06-18 19:18:42 -0400

    refactored SubredditTableViewController by removing unused code and autogenerated comments

--- RefectoringLSB/SubredditTableViewController.swift
+++ RefectoringLSB/SubredditTableViewController.swift
@@ -46,11 +46,6 @@ class SubredditTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         let headerView = Bundle.main.loadNibNamed("TableHeaderView", owner: nil, options: nil)!.first as! TableHeaderView
         headerView.textField.text = SubReddit.aww.rawValue
         tableView.tableHeaderView = headerView
@@ -94,22 +89,12 @@ class SubredditTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-      super.viewWillTransition(to: size, with: coordinator)
-      DispatchQueue.main.async {
-        self.tableView.tableHeaderView?.layoutIfNeeded()
-        self.tableView.tableHeaderView = self.tableView.tableHeaderView
-      }
-    }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return posts.count
     }
 
@@ -125,56 +110,16 @@ class SubredditTableViewController: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        
-        guard let modal = segue.destination as? FullImageViewController, let cell = sender as? PostCell else {
+        guard let modal = segue.destination as? FullImageViewController,
+              let cell = sender as? PostCell,
+              let indexPath = tableView.indexPath(for: cell) else {
+                
             return
         }
         
-        let indexPath = tableView.indexPath(for: cell)!
-        
         modal.image = posts[indexPath.row].image
-        
     }
     
 }
