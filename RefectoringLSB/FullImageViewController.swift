3b35f1b4143588846d63d7d67c7e42c9bd85e9d5
Matt Dias <matt.dias6@gmail.com>
2020-06-18 19:03:40 -0400

    refactored FullImageViewController by removing unnecessary code

--- RefectoringLSB/FullImageViewController.swift
+++ RefectoringLSB/FullImageViewController.swift
@@ -16,19 +16,6 @@ class FullImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         imageView.image = image
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
