6d78d2f260f9d44bea4813cf64e47f2e8c225a2f
Matt Dias <matt.dias6@gmail.com>
2020-06-18 19:01:31 -0400

    refactored PostCell by removing unnecessary code

--- RefectoringLSB/PostCell.swift
+++ RefectoringLSB/PostCell.swift
@@ -9,20 +9,6 @@
 import UIKit
 
 class PostCell: UITableViewCell {
-    
     @IBOutlet weak var postImage: UIImageView!
-    
     @IBOutlet weak var postTitle: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
